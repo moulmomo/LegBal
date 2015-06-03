a4b490c66e5bef556566cae78f0037a715442c67
moulmomo <moulmomo@users.noreply.github.com>
2015-06-03 14:40:34 +0200

    Create Localisation.java

---
+++ Localisation.java
@@ -0,0 +1,18 @@
+package com.example.mohamed.legapp2;
+
+/**
+ * Created by Mohamed on 31/05/2015.
+ */
+public class Localisation {
+
+    public String major;
+    public String minor;
+    public String nom;
+
+    public Localisation(String nom, String major, String minor){
+        this.nom = nom;
+        this.major = major;
+        this.minor = minor;
+    }
+
+}
